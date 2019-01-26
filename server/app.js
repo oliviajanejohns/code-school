ea9147d3f7cb503e232a8ae41852348b2acbbe8a
Olivia Johnson <o.johnson.1@warwick.ac.uk>
2019-01-26 14:46:07 +0000

    fixed requests

--- server/app.js
+++ server/app.js
@@ -1,74 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const url = 'mongodb://localhost/database';
-
-const User = require('./model/user');
-
-// Initialise our app variable
-const app = express()
+require('rootpath')();
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
 
 //Middleware for bodyparsing using json
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
 
-// Check User Already Exists
-app.post('/api/user/login', (req, res) => {
-  mongoose.connect(url,{ useMongoClient: true }, function(err){
-      if(err) throw err;
-      User.find({
-          username : req.body.username, password : req.body.password
-      }, function(err, user){
-          if(err) throw err;
-          if(user.length === 1){  
-              return res.status(200).json({
-                  status: 'success',
-                  data: user
-              })
-          } else {
-              return res.status(200).json({
-                  status: 'fail',
-                  message: 'Login Failed',
-              })
-          }
-      })
-  });
-})
+// use JWT auth to secure the api, the token can be passed in the authorization header or querystring
+app.use(expressJwt({
+    secret: config.secret,
+    getToken: function (req) {
+        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+            return req.headers.authorization.split(' ')[1];
+        } else if (req.query && req.query.token) {
+            return req.query.token;
+        }
+        return null;
+    }
+}).unless({ path: ['/users/authenticate', '/users/register'] }));
 
-// Add New User to Mongoose
-app.post('/api/user/signUp', (req, res) => {
-    mongoose.connect(url, { useMongoClient: true }, function(err){
-        if(err) throw err;
-        const user = new User({
-            username: req.body.username,
-            password: req.body.password
-        })
-        user.save((err, doc) => {
-            if(err) throw err;
-            return res.status(200).json({
-                status: 'success',
-                data: doc
-            })
-        })
-    });
-})
+// routes
+app.use('/users', require('./controllers/users.controller'));
 
-app.post('/api/user/delete', (req, res) => {
-	mongoose.connect(url, { useMongoClient: true }, function(err){
-		if(err) throw err;
-		Post.findByIdAndRemove(req.body.id,
-			(err, doc) => {
-			if(err) throw err;
-			return res.status(200).json({
-				status: 'success',
-				data: doc
-			})
-		})
-	});
-})
+// error handler
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send('Invalid Token');
+    } else {
+        throw err;
+    }
+});
 
 // HTTP listener
-app.listen(3000, function () {
-  console.log('Listening on port 3000');
+var port = process.env.NODE_ENV === 'production' ? 80 : 3000;
+var server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
 
